e05c3b01aa20f8c832a3425dee9b4419640abd3c
Tangi Le Henaff <lehenafftangi@gmail.com>
2025-03-26 15:58:20 +0100

    modification du python

--- python/testDP.py
+++ python/testDP.py
@@ -2,26 +2,41 @@ import os
 import subprocess
 from pathlib import Path
 #os.system("touch output.csv")
-root = Path("./convert/")
+root = Path("./instances/")
 buildPath="./Knapsack/build"
 #os.system("buildPath+"/src/Heur convert/kplib-master/00Uncorrelated/n00050/R01000/s000.kp ")
 #test=subprocess.check_output("buildPath+"/src/BB "+"convert/kplib-master/00Uncorrelated/n00050/R01000/s000.kp ", shell=True)
 #print(str(test).replace('\\n','\n'))
 baseFileName="resDP"
-i=0
 
+
+numFile=Path("./resultats/numero.conf")
+if not(os.path.isfile(numFile)):
+    print("creation fichier de conf")
+    num="1"
+else:
+    print("fichier de configuration trouve")
+    num=""
+fich=open(str(numFile),"a")
+fich.write(num)
+fich.close()
+fich=open(str(numFile),"r")
+nb=fich.read(1)
+fich.close()
+fich=open(str(numFile),"w")
+fich.write(str(int(nb)+1))
+fich.close()
 #creation du nouveau fichier de res (le fichier 'nb' sert à mémoriser le chiffre courant)
-nb=subprocess.check_output("ls resultats/ | grep ^[0-9]$",shell=True,errors=False)
-nb_nouv=(int(nb[:1])+1)
-nb=int(nb[:1])
-subprocess.check_output("touch resultats/"+baseFileName+""+str(nb_nouv)+".csv",shell=True,errors=False)
-subprocess.check_output("mv resultats/"+str(nb)+" resultats/"+str(nb_nouv)+"",shell=True,errors=False)
-var=subprocess.check_output("echo \"file name;TYPE;cost;upper bound;rounded upper bound;gap;time\" > resultats/"+baseFileName+""+str(nb_nouv)+".csv",shell=True,errors=False)
+#nb=subprocess.check_output("ls resultats/ | grep ^[0-9]$",shell=True,errors=False)
 
+subprocess.check_output("touch resultats/"+baseFileName+""+str(nb)+".csv",shell=True,errors=False)
+var=subprocess.check_output("echo \"file name;TYPE;cost;upper bound;rounded upper bound;gap;time\" > resultats/"+baseFileName+""+str(nb)+".csv",shell=True,errors=False)
+
+i=0
 for path in root.glob("**/*"):
     if(str(path)[len(str(path))-2:]=="kp" or str(path)[len(str(path))-2:]=="in" ):
         try:
-            test=subprocess.check_output(buildPath+"/DP ./"+str(path)+" resultats/"+baseFileName+""+str(nb_nouv)+".csv", shell=True,errors=False)
+            test=subprocess.check_output(buildPath+"/DP ./"+str(path)+" resultats/"+baseFileName+""+str(nb)+".csv", shell=True,errors=False)
             #print("./"+str(path))
             i+=1
         except:
@@ -32,5 +47,5 @@ for path in root.glob("**/*"):
     if(i==1): #NB max d'instances à tester
         break
 
-    print("fichier de sortie : resultats/"+baseFileName+""+str(nb_nouv)+".csv")
+print("fichier de sortie : resultats/"+baseFileName+""+str(nb)+".csv")
 
